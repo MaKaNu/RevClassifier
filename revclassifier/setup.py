393e896fcaaf785f19265d280ba1b78034ba4250
Matti <matti.kaupenjohann@fh-dortmund.de>
2021-12-02 10:00:53 +0100

    Add Base structure

---
+++ revclassifier/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='revclassifier',
+    version='0.1',
+    description='',
+    url='',
+    author='',
+    author_email='',
+    license='',
+    packages='',
+    zip_safe=False
+)
