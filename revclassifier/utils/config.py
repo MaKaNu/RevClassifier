d0d9233d8a7226ccd762ab732604cbaf223f0bec
Matti <matti.kaupenjohann@fh-dortmund.de>
2021-12-02 11:54:11 +0100

    Add config

---
+++ revclassifier/utils/config.py
@@ -0,0 +1,28 @@
+# Builtin imports
+from pathlib import Path
+import json
+
+# Thirdparty imports
+from pydantic import BaseModel
+from pydantic.class_validators import validator
+
+
+class Config(BaseModel):
+    path = Path("")
+
+    def load_json(self, config_file=Path("revclassifier/config.json")):
+        with config_file.open() as json_file:
+            config_data = json.load(json_file)
+            print(config_data)
+            json.dumps(config_data, indent=4)
+
+    @validator('path')
+    def check_path_existence(cls, val):
+        if not val.is_dir():
+            msg = colored(255, 85, 85, f"Directory \"{val}\" does not exists")
+            raise ValueError(msg)
+        return val
+
+
+def colored(r, g, b, text):
+    return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text)
